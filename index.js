96725735de7ea7b9acb862208e7a54f3a493dc46
Effren Anthony <effrenanthony2704@gmail.com>
2019-04-06 12:02:56 -0500

    backCodijobModify3

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const proyectoskill_1 = require("./api/routes/proyectoskill");
 const persona_1 = require("./api/routes/persona");
 const usuario_1 = require("./api/routes/usuario");
 const auth_1 = require("./api/routes/auth");
+const home_1 = require("./api/routes/home");
 //Importamos la libreria desde node modules (require para importar la libreria en node_modules)
 var express = require('express');
 // app es una instancia directa de express
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 const PUERTO = process.env.PORT || 3700;
 //importando las rutas
 app.use('/api', skill_1.router_skill);
+app.use('/', home_1.router_home);
 app.use('/api', proyecto_1.router_proyecto);
 app.use('/api', proyectoskill_1.router_proyectoSkill);
 app.use('/api', persona_1.router_persona);
